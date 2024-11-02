ca2bb3cc08e0c75ffbe34eb2ba3c1d8c95978115
felipe escobar <pipesco93@gmail.com>
2024-11-02 09:35:08 -0500

    added new clas data_student that inherists atributes from students class

---
+++ lewagon/data_student.py
@@ -0,0 +1,20 @@
+from student import Student
+
+# lewagon/data_student.py
+
+# the import will only work if executed from lewagon-project directory
+# unless you have set up the PYTHONPATH
+
+class DataStudent(Student):
+    cursus = 'datascience'
+    def __init__(self, name, age, batch):
+        super().__init__(name, age)
+        self.batch = batch
+
+
+boris = DataStudent('boris', 30, 1767)
+boris.batch
+
+print(boris.name)
+print(boris.age)
+boris.says(f"he is new to batch: {boris.batch}")
